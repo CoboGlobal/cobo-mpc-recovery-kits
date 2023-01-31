2de32845a0c1cb7794e9d0368e15fc0fc287f8d3
evallozhe <51144526+evallozhe@users.noreply.github.com>
2023-01-31 10:19:42 +0800

    Add version subcommand and update to v0.1.1 (#4)
    * Specify binary archives
    
    * Add version subcommand
    
    * Release v0.1.1
    
    ---------
    
    Co-authored-by: Zhe Zhao <zhaozhe@cobo.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/cobo/cobo-mpc-recovery-kits/version"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -18,15 +20,16 @@ var (
 
 func InitCmd() {
 	rootCmd.AddCommand(verifyCmd)
+	rootCmd.AddCommand(versionCmd)
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 }
 
 func AddFlag() {
-	rootCmd.PersistentFlags().StringSliceVar(&GroupFiles, "group-recovery-files", []string{},
+	rootCmd.Flags().StringSliceVar(&GroupFiles, "group-recovery-files", []string{},
 		"TSS group recovery files, such as recovery/tss-group-id-node-1-time1,recovery/tss-group-id-node-2-time2")
-	rootCmd.MarkPersistentFlagRequired("group-recovery-files")
-	rootCmd.PersistentFlags().StringVar(&GroupID, "group-id", "", "recovery group id")
-	rootCmd.MarkPersistentFlagRequired("group-id")
+	rootCmd.MarkFlagRequired("group-recovery-files")
+	rootCmd.Flags().StringVar(&GroupID, "group-id", "", "recovery group id")
+	rootCmd.MarkFlagRequired("group-id")
 
 	rootCmd.Flags().BoolVar(&ShowRootPrivate, "show-root-private-key", false, "show TSS root private key")
 	rootCmd.Flags().StringSliceVar(&Paths, "paths", []string{}, "key HD derivation paths")
@@ -34,6 +37,12 @@ func AddFlag() {
 		"address csv file, contains HD derivation paths")
 	rootCmd.Flags().StringVar(&CsvOutputDir, "csv-output-dir", "recovery",
 		"address csv output dir, derive keys file output in this directory")
+
+	verifyCmd.Flags().StringSliceVar(&GroupFiles, "group-recovery-files", []string{},
+		"TSS group recovery files, such as recovery/tss-group-id-node-1-time1,recovery/tss-group-id-node-2-time2")
+	verifyCmd.MarkFlagRequired("group-recovery-files")
+	verifyCmd.Flags().StringVar(&GroupID, "group-id", "", "recovery group id")
+	verifyCmd.MarkFlagRequired("group-id")
 }
 
 var rootCmd = &cobra.Command{
@@ -41,6 +50,7 @@ var rootCmd = &cobra.Command{
 	Short: "cobo-mpc-recovery-tool to reconstruct root private key by TSS group recovery files and derive child keys",
 	Args:  cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Version: " + version.TextVersion() + "\n")
 		err := checkFlags()
 		if err != nil {
 			log.Fatal("Check flags failed: ", err)
---
+++ version/version.go
@@ -0,0 +1,15 @@
+package version
+
+import "fmt"
+
+// These constants define the application version.
+const (
+	Major = 0
+	Minor = 1
+	Patch = 1
+)
+
+func TextVersion() string {
+	version := fmt.Sprintf("%d.%d.%d", Major, Minor, Patch)
+	return version
+}
